544a93cdd91c387a394614e399a36d3fff655f12
Ilushling <7870818+Ilushling@users.noreply.github.com>
2023-12-23 23:02:24 +0000

    fix(package.json): add main

---
+++ index.js
@@ -0,0 +1,12 @@
+export { default as Edge } from './src/Edge.js';
+export { default as Graph } from './src/Graph.js';
+export { default as Node } from './src/Node.js';
+
+export { default as DoublyLinkedList } from './src/Lists/DoublyLinkedList.js';
+export { default as DoublyLinkedListNode } from './src/Lists/DoublyLinkedListNode.js';
+export { default as LinkedList } from './src/Lists/LinkedList.js';
+export { default as LinkedListNode } from './src/Lists/LinkedListNode.js';
+
+export { default as AvlTree } from './src/Trees/AvlTree.js';
+export { default as BinarySearchTree } from './src/Trees/BinarySearchTree.js';
+export { default as BinarySearchTreeNode } from './src/Trees/BinarySearchTreeNode.js';
